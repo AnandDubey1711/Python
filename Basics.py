d5618c32a1e6542dd364c2466f41fc8d1d4ce0ed
Anand Dubey <anand.kumar.dubey.cse@adgitmdelhi.ac.in>
2024-04-11 12:18:33 +0530

    first-commit

---
+++ Basics.py
@@ -0,0 +1,35 @@
+# Python is general purpose high level programming language which focusses on creating a user friendly syntax for programming purposes
+# It works on the concept of indentation which is easy to understand and helps in readability
+# It is an interpreted language which means each line is read one after the another instead of compiling the whole of the program at once.
+# It was created back in the 1990s 
+# Python is one of the most popular programming lanugage around the globe owing to facts such as it high level of versatality and libraries.
+# Like always I will start the coding by writing the globally accepted paradigm which is printing the Hello World
+
+print("Hello World")
+# Python does not requires any terminator and print is used to print the data
+# There are various data types in python as well which helps in carrying out various operations
+# Number, String, Boolean, Float, 
+
+name = "Anand" # String
+age = 23 # Integer
+isPass = True #boolean
+percentage = 85.34 # float
+grade = 'A' # String as well as there is not any separate type of the Character type
+
+
+# To check the type of a variable in Python type method is used
+print(type(name))  #<class 'str'>
+print(type(age))  # <class 'int'>
+print(type(isPass)) # <class 'bool'>
+print(type(percentage)) # <class 'float'>
+
+# Printing the values together
+print("My name is",name,"and my age is",age,". My grade is",grade,"and my pass status is",isPass,"with my percentage being",percentage)
+
+# Now we will look into multiple assignment which allows us to assign variables at once instead assigning one by one
+
+myName, myAge, myGrade = "Anand",23,'A'
+# There is another method to assign multiple variabes with a single value
+math, english, science = 45,34,34
+print(myName, myAge, myGrade)
+print(math,english,science)
---
+++ Strings.py
@@ -0,0 +1,26 @@
+# Now shifting focus towards the string methods
+name = 'Captain America'
+
+# To get the length of the string we use len method
+print(len(name))
+
+# To check if a value exits in a string on not
+print(name.find("C")) # Returns the first index of C - 0
+
+# To capitalize the name
+print(name.capitalize())
+
+# To change into upper case upper is used
+print(name.upper())
+
+# To change into lower case lower is used
+print(name.lower())
+
+# To check if the a string contains only digit or not
+print(name.isdigit())
+
+# To check if the string contains only character or not
+print(name.isalpha())
+
+# To count the occurence of a value in a string
+print(name.count('a'))
